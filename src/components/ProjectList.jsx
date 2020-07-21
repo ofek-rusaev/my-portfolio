8ed5f95c71cde1bf60715c1e565da75327979df6
ofek-rusaev <ofek2602@gmail.com>
2020-07-22 00:18:54 +0300

    test

--- src/components/ProjectList.jsx
+++ src/components/ProjectList.jsx
@@ -3,7 +3,7 @@ import ProjectPreview from './ProjectPreview';
 
 const ProjectList = (props) => {
     const { projects } = props;
-    console.log('num of projects', projects);
+    // console.log('num of projects', projects);
     return (<>
         {projects.map(project => <ProjectPreview project={project} key={project.id} />)}
     </>);
